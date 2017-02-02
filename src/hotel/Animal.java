afb86193c63bc322ec51038d6dcb1b7aaf0fa4ef
CARLOS EDUARDO BARBOSA DE ARAUJO 116111239 <carloseba@lcc2-34.lcc.ufcg.edu.br>
2017-02-02 11:33:50 -0300

    fiz a classe animal, completa ai o resto.

--- src/hotel/Animal.java
+++ src/hotel/Animal.java
@@ -1,5 +1,78 @@
 package hotel;
 
 public class Animal {
+	public String nome;
+	public String tipo;
+	public int idade;
+	
+	public Animal(String nome, String tipo, int idade) throws Exception{
+		if(nome == null || nome == "") throw new Exception ("Nome não pode ser vazio ou nulo.");
+		if(tipo == null || tipo == "") throw new Exception ("Tipo não pode ser vazio ou nulo.");
+		if(idade == 0) throw new Exception ("Idade não pode ser 0.");
+		this.nome = nome;
+		this.tipo = tipo;
+		this.idade = idade;
+		
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		result = prime * result + ((tipo == null) ? 0 : tipo.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Animal other = (Animal) obj;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		if (tipo == null) {
+			if (other.tipo != null)
+				return false;
+		} else if (!tipo.equals(other.tipo))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Animal [nome=" + nome + ", tipo=" + tipo + ", idade=" + idade + "]";
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
 
 }
